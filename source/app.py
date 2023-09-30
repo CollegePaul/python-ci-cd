882779246c0c024b1fa7cbc0839cc8db1bc7c091
CollegePaul <psmith@rnngroup.ac.uk>
2023-09-30 17:14:08 +0100

    fixing linting errors

--- source/app.py
+++ source/app.py
@@ -2,14 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
     return "Hello, World!"
 
 
-
-#ensure this is the main application
+# ensure this is the main application
 if __name__ == "__main__":
     app.run()
 
